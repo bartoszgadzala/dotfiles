ba0c19af3f60b6d83c267b863aa1a295d0d47def
Bartosz Gadzała <bartosz.gadzala@gmail.com>
2014-02-02 23:27:05 +0100

    Scripts minor changes

--- bin/tmux_default.sh
+++ bin/tmux_default.sh
@@ -4,10 +4,11 @@ SESSION=$USER
 
 tmux has-session -t $SESSION || {
 	echo Creating session
-	tmux new-session -d -s $SESSION
-	tmux new-window -t $SESSION:1 -n 'News' 'canto'
-	tmux new-window -t $SESSION:2 -n 'Music' 'ncmpcpp'
+	tmux new-session -d -s $SESSION -n 'News' 'canto'
+	tmux new-window -t $SESSION:1 -n 'Music' 'ncmpcpp'
+	tmux new-window -t $SESSION:2 -n 'Mail' 'mutt'
 	tmux new-window -t $SESSION:3 -n 'IM' 'mcabber'
-	tmux select-window -t $SESSION:0
+	tmux new-window -t $SESSION:4
+	tmux select-window -t $SESSION:4
 }
 tmux -2 attach-session -t $SESSION
